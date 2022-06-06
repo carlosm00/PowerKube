eeffddd44cbfcb517c3a91458ea57d062d738068
Carlos Mena <menacarlos.asir@gmail.com>
2022-06-07 00:26:49 +0200

    Main functions created

---
+++ powerkube.ps1
@@ -0,0 +1,66 @@
+<#
+   .Description
+   Project: PowerKube
+   Author/Autor: Carlos Mena (https://github.com/carlosm00)
+   Definition: PowerShell application for Kubernetes (K8s) easy deployment and management. 
+      This is an alternative for pythonic solution easyKube (https://github.com/carlosm00/easyKube)
+
+#>
+
+# .Namespace setting
+# We set namespace at the beginning of the application. It is set to 'powerkube' as default option. 
+# If there is need of changing the namespace of the application read, change it on the variable below
+
+$namespace = "easykube"
+kubectl config set-context --current --namespace=$namespace
+
+# .Options setup
+## Deployments
+### List all deployments from namespace
+function List-Deployment {
+    kubectl get deployment --namespace=$namespace
+}
+### Describe all deployments from namespace
+function Describe-Deployment {
+    kubectl describe deployment --namespace=$namespace
+}
+### Create deployment from file
+function Create-Deployment {
+	param(
+        [Parameter( Mandatory = $true,
+                    Position=0,
+                    ValueFromPipeline=$true )]
+					[ValidateNotNullOrEmpty()]
+					[string]$file
+	)	
+    kubectl create -f $file
+}
+
+## Services
+### GET services from namespace
+function List-Services {
+    kubectl get services --namespace=$namespace
+}
+
+### Create service from file
+function Create-Service {
+	param(
+        [Parameter( Mandatory = $true,
+                    Position=0,
+                    ValueFromPipeline=$true )]
+					[ValidateNotNullOrEmpty()]
+					[string]$file
+	)	
+    kubectl create -f $file
+}
+
+## Pods
+### GET all pods from namespace
+function List-Pods {
+    kubectl get pods --namespace=$namespace
+}
+
+### Describe pods from namespace
+function Describe-Pods {
+    kubectl describe pods --namespace=$namespace
+}
